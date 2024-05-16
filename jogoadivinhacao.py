e35ad2894a9b74a082913027f58af3e144c55ca1
gabriel gostoso <128156868+gabrielfernandes777@users.noreply.github.com>
2024-05-16 09:07:09 -0300

    Add files via upload

--- jogoadivinhacao.py
+++ jogoadivinhacao.py
@@ -35,13 +35,13 @@ while(rodada <= numeroTentativas):
 
 #Declarando as condições
     if (numeroSecreto == chute):
-        print('Você acertou!')
+        print('Você acertou! E sua pontuaçao foi:' ,pontos)
         break
     elif(chute>numeroSecreto):
         print('Você errou!! O número secreto é um número menor')
     else:
         print('Você errou!! O número secreto é um número maior')
-        pontos_perdidos = abs(numeroSecreto - chute);
+        pontos_perdidos = abs(numeroSecreto - chute)
         pontos = pntos - pontos_perdidos
 
     #numeroTentativas = numeroTentativas - 1
